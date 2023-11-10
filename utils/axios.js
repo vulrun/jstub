7b63bf935ef59c81324e2a676fc5e980f32ac059
Mohit <vulrun@gmail.com>
2023-11-10 13:40:59 +0530

    fix: axios fixes

--- utils/axios.js
+++ utils/axios.js
@@ -7,6 +7,10 @@ axios.interceptors.response.use(responseHandler, responseErrorHandler);
 
 module.exports = axios;
 
+function isAxiosError(error) {
+  return axios.isAxiosError(error) || error?.name === "AxiosError";
+}
+
 function requestHandler(config) {
   if (!config?.headers["Content-Type"]) {
     config.headers["Content-Type"] = "application/json";
@@ -25,38 +29,32 @@ function requestErrorHandler(error) {
 }
 
 function responseHandler(response) {
-  const config = response?.config;
-
-  if (config?.raw) {
+  if (response?.config?.raw) {
     return response;
   }
 
-  if (response?.status == 200) {
+  if (/20[0-8]/.test("" + response?.status)) {
     const data = response?.data;
-    if (!data) throw new HttpError("NO_API_DATA");
+    if (!data) throw new Error("NO_API_DATA");
 
     return data;
   }
-  throw new HttpError("INVALID_API_STATUS_CODE");
+  throw new Error("INVALID_API_STATUS_CODE");
 }
 
 function responseErrorHandler(response) {
-  const config = response?.config;
-  if (config?.raw) {
+  if (response?.config?.raw) {
     return response;
   }
 
-  console.log(response);
   return httpErrorHandler(response);
 }
 
 function httpErrorHandler(error) {
   if (error === null) throw new Error("UNRECOVERABLE_ERROR");
 
-  if (_axios.isAxiosError(error)) {
-    const response = error?.response;
-    const request = error?.request;
-    const config = error?.config;
+  if (isAxiosError(error)) {
+    const { config, request, response } = error;
 
     if (error.code === "ERR_NETWORK") {
       throw new Error("API_CONNECTION_PROBLEMS");
@@ -75,6 +73,7 @@ function httpErrorHandler(error) {
         throw new Error("UNAUTHORIZED_API_REQUEST");
         // redirect user to login
       }
+      return error?.response?.data;
     } else if (request) {
       //The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in Node.js
     }
